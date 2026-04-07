b3767b06e58ce59a228359f027ce974aabc0c2a3
theelusivejustin <dcbang747@gmail.com>
2026-04-07 11:32:15 -0700

    updated with Dev Name and also new test because I want to explore things

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "David Chang";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "dcbang747";
     }
 
     /**
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,10 +24,14 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         // TODO: Replace Chris G. with your name as shown on
         // <https://bit.ly/cs156-s26-teams>
-        assertEquals("Chris G.", Developer.getName());
+        assertEquals("David Chang", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test // Checks Git Hub ID is correct
+    public void getGithubId_returns_correct_github_id() {
+        assertEquals("dcbang747", Developer.getGithubId());
+    }
+    
 }
